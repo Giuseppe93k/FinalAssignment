149dee7caca2474eee1185ceb50e7dc452acc714
Giuseppe93k <164879856+Giuseppe93k@users.noreply.github.com>
2024-08-26 10:50:00 +0100

    Add project files.

---
+++ FinalAssignment/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+//function to change color onmouseover 
+function changeColor() {
+    document.getElementById("color_change").style.color = "darkblue";
+}
+
+//color back to original when mouseout
+function originalColor() {
+    document.getElementById("color_change").style.color = "darkgrey";
+}
